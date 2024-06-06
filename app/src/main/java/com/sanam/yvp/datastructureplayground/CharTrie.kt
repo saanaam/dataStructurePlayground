da051b9a8931ce4daa7e9cf88fca825d9987c342
sanam yavarpour <sanamyavarpor@gmail.com>
2024-06-06 16:53:29 +0200

    add CharTrie to create a tree for Dictionary

--- app/src/main/java/com/sanam/yvp/datastructureplayground/CharTrie.kt
+++ app/src/main/java/com/sanam/yvp/datastructureplayground/CharTrie.kt
@@ -4,23 +4,27 @@ fun dictionary() {
     val dictionary = CharTrie()
     dictionary.insert("cat")
     dictionary.insert("can")
+    dictionary.insert("cow")
+    dictionary.insert("Bowl")
     println("Done!")
 }
 
 class CharTrie {
-    private val dictionary = TreeNode(' ')
-
-    var isEnd: Boolean = false
-
-    private fun index(char: Char) = char.lowercaseChar() - 'a' + 1
-
+    private val root = Node(' ')
+    private class Node(val value: Char) {
+        companion object {
+            const val ALPHABET_SIZE = 26
+        }
+        val children: Array<Node?> = arrayOfNulls(ALPHABET_SIZE)
+    }
     fun insert(word: String) {
-        var current = dictionary
+        var current = root
         word.forEach {
-            if (dictionary.children.getOrNull(index(it)) == null) {
-                current.add(TreeNode(it))
+            val index = it.lowercaseChar() - 'a' + 1
+            if (current.children[index] == null) {
+                current.children[index] = Node(it)
             }
-            current = current.
+            current = current.children[index]!!
         }
     }
 }
