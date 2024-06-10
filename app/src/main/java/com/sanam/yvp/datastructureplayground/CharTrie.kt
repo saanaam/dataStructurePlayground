9139efb73a5e6ef386fb5d706202ad0925590484
sanam yavarpour <sanamyavarpor@gmail.com>
2024-06-10 07:15:06 +0200

    Update  CharTrieByHashtable
    Add isEndOfTheWord value

--- app/src/main/java/com/sanam/yvp/datastructureplayground/CharTrie.kt
+++ app/src/main/java/com/sanam/yvp/datastructureplayground/CharTrie.kt
@@ -3,10 +3,27 @@ package com.sanam.yvp.datastructureplayground
 fun dictionary() {
     val dictionary = CharTrieByHashtable()
     dictionary.insert("cat")
-    dictionary.insert("can")
     dictionary.insert("cow")
+    dictionary.insert("canada")
     dictionary.insert("Bowl")
     println("Done!")
+    println("contains cat? ${dictionary.contains("cat")}")
+    println("contains can? ${dictionary.contains("can")}")
+    println("contains canada? ${dictionary.contains("canada")}")
+    println("contains ..? ${dictionary.contains("..")}")
+    println("contains null? ${dictionary.contains(null)}")
+    println("contains bow? ${dictionary.contains("bow")}")
+    println("contains bowl? ${dictionary.contains("bowl")}")
+}
+
+fun testTrieTraversal() {
+    val dictionary = CharTrieByHashtable()
+    dictionary.insert("care")
+    dictionary.insert("cat")
+    dictionary.insert("can")
+    dictionary.preOrderTraverse()
+    println()
+    dictionary.postOrderTraverse()
 }
 
 //This solution is not optimized because by creating each Node we allocate an array with 26 size.
@@ -24,10 +41,10 @@ class CharTrie {
 
     fun insert(word: String) {
         var current = root
-        word.forEach {
-            val index = it.lowercaseChar() - 'a' + 1
+        word.forEach { char ->
+            val index = char.lowercaseChar() - 'a' + 1
             if (current.children[index] == null) {
-                current.children[index] = Node(it)
+                current.children[index] = Node(char)
             }
             current = current.children[index]!!
         }
@@ -36,20 +53,82 @@ class CharTrie {
 
 // this implementation has a problem in object oriented design
 class CharTrieByHashtable {
+
     private val root = Node(' ')
 
-    private class Node(val value: Char) {
-        val children = HashTable<Int, Node>()
+    class Node(val value: Char) {
+        val children = HashMap<Int, Node>()
+        var isEndOfTheWord = false
+        fun setEndOfTheWord(index: Int, word: String) {
+            if (index == word.length - 1) {
+                isEndOfTheWord = true
+            }
+        }
     }
+
 
     fun insert(word: String) {
         var current = root
-        word.forEach {
-            val index = it.lowercaseChar() - 'a' + 1
-            if (current.children.get(index) == null) {
-                current.children.put(index ,Node(it) )
+        word.forEachIndexed { i, value ->
+            val index = value.lowercaseChar() - 'a' + 1
+            if (current.children[index] == null) {
+                current.children[index] = Node(value)
             }
-            current = current.children.get(index)!!
+            current = current.children[index]!!
+            current.setEndOfTheWord(i, word)
+        }
+    }
+
+    fun contains(word: String?): Boolean {
+        var exist = false
+        var current = root
+        word?.forEachIndexed { i, value ->
+            val index = value.lowercaseChar() - 'a' + 1
+            if (current.children.containsKey(index)) {
+                current = current.children[index]!!
+                if (i == word.length - 1) {
+                    exist = current.isEndOfTheWord
+                }
+            } else {
+                return false
+            }
+        }
+        return exist
+    }
+
+    //Visit the current Node.. recursively visit the left and right child
+    //print all the word
+    fun preOrderTraverse() {
+        preOrderTraverse(root)
+    }
+
+    //Recursively visit the left and right child, only visit the current node after the left and right
+    //child have been visited
+    //delete a word at trie post
+    fun postOrderTraverse() {
+        postOrderTraverse(root)
+    }
+
+    private fun preOrderTraverse(root: Node) {
+        println(root.value)
+        root.children.values.forEach {
+            preOrderTraverse(it)
+        }
+    }
+
+    private fun postOrderTraverse(root: Node) {
+        root.children.values.forEach {
+            postOrderTraverse(it)
+        }
+        println(root.value)
+    }
+
+    fun delete(word: String) {
+        if (!contains(word)) {
+            println("Word does not exist")
+        } else {
+
         }
     }
 }
+
